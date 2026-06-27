7c8cc656963fc5363d8e837c4766d7afbfca8f8b
ARCH <rumedhaneshara1@gmail.com>
2026-06-27 18:15:17 +0530

    Create brute_force.py
    Add automated brute force simulation script

---
+++ brute_force.py
@@ -0,0 +1,9 @@
+# Automated Brute-Force PIN Guessing Simulator
+# Built by Rumedha for ANC Pre-Semester Training
+
+secret_pin = 4
+guess = 1
+
+while guess <= secret_pin:
+    print("Testing PIN:", guess)
+    guess = guess + 1
